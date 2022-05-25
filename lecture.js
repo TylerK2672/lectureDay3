93a75f77eddf4c07e30028e5d28687b8e9db5386
Tyler Kendall <tylerchristiankendall@gmail.com>
2022-05-25 12:17:45 -0600

    Initial commit

---
+++ lecture.js
@@ -0,0 +1,15 @@
+jonSnowAttack = 25;
+jamieLannisterAttack = 35;
+
+if (jonSnowAttack > jamieLannisterAttack) 
+{
+    console.log("Jon Snow has better attack than Jamie Lannister");
+} 
+else if (jonSnowAttack === jamieLannisterAttack)
+{
+    console.log("Jon Snow and Jamie Lannister have the same attack power");
+} 
+else 
+{
+    console.log("Jamie Lannister has better attack than Jon Snow");
+}
